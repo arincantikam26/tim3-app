674e3d91fc30924f7763b6227891fb234e33b4e4
rizkiah <rizkiahrahmahhatta19@gmail.com>
2022-12-29 15:07:34 +0700

    View Halaman Prodi & Jurusan [rizkiah]

---
+++ database/migrations/2022_12_29_062755_create_prodis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_prodi', function (Blueprint $table) {
+            $table->increments('id_prodi');
+            $table->foreignId('jurusan_id');
+            $table->string('nama_prodi')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_prodi');
+    }
+};
