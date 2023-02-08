c2e9f1ac1c8cd635ce31ec26da84a3f04673c776
arincantikam26 <arincantikam@gmail.com>
2023-02-09 06:40:43 +0700

    add hasil pemilihan prodi

--- app/Http/Controllers/ProsesController.php
+++ app/Http/Controllers/ProsesController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hasil;
 use App\Models\Jurusan;
 use App\Models\JurusanSekolah;
 use App\Models\Kriteria;
 use App\Models\Pertanyaan;
 use App\Models\Pilihan;
+use App\Models\Preferensi;
 use App\Models\Prodi;
 use Illuminate\Http\Request;
 
@@ -39,14 +41,60 @@ class ProsesController extends Controller
         return view('users.proses.pertanyaan', compact('title','active','prodi','kriteria','pertanyaan'));
     }
 
-    public function hasilProdi(Request $request) {
-        
-        return view('users.proses.hasil_pertanyaan');
-    }
-
-    public function hasilPertanyaan(Request $request) {
+    public function hasilPilihan(Request $request) {
         $title = 'Dashboard';
         $active = 'user';
-        return view('users.proses.hasil_pertanyaan', compact('title', 'active'));
+        $maxArr = array();
+        $avgArr = array();
+
+        $pre = Preferensi::all();
+        if ($pre->count()) {
+            $metoda = Hasil::metoda();
+        } else {
+            $metoda = '';
+        }
+
+        $ahp = [
+            'nama' => $metoda['namaKriteria'],
+            'eigen' => $metoda['eigen'],
+            'totaleigen' => $metoda['totaleigen']
+        ];
+
+        $data = $request->get('data');
+        // $data[0]['Minat'][0]
+
+        for ($a = 0; $a < count($data); $a++) {
+            for($i = 0; $i < count($data[$a])-1; $i++){
+                $max = 0;
+                $avg = 0;
+                for ($j = 0; $j < count($data[$a][$ahp['nama'][$i]]); $j++){
+                    $avg = $avg + $data[$a][$ahp['nama'][$i]][$j];
+
+                    if ($j == (count($data[$a][$ahp['nama'][$i]]) - 1)) {
+                        $avgArr[$a][$ahp['nama'][$i]] = $avg / ($j + 1); // set nilai rata2 dari masing2 kriteria
+                        if (($avg / ($j + 1)) > $max) {
+                            $max = $avg / ($j + 1);
+                            var_dump($max);
+                        }
+                    }
+                }
+                $maxArr[$ahp['nama'][$i]] = $max; // set nilai max dari setiap kriteria yg ada
+            }
+        }
+
+        $next = array();
+        for ($i = 0; $i < count($data); $i++) {
+            for ($j = 0; $j < count($ahp['nama']); $j++) {
+                $next[$i][$j] = $avgArr[$i][$ahp['nama'][$j]] / $maxArr[$ahp['nama'][$j]];
+            }
+        }
+
+        $saw = [
+            'data' => $data,
+            'max_value' => $maxArr,
+            'next' => $next
+        ];
+    
+        return view('users.proses.hasil_pilihan', compact('title', 'active', 'ahp', 'saw'));
     }
 }
