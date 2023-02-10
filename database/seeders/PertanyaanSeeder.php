747119839ca6654e47389bb5c2dcb7f037f25881
utarirezalf <urezkyalfany@gmail.com>
2023-02-10 10:11:07 +0700

    seeder pertanyaan

--- database/seeders/PertanyaanSeeder.php
+++ database/seeders/PertanyaanSeeder.php
@@ -53,19 +53,14 @@ class PertanyaanSeeder extends Seeder
                 'pertanyaan' => 'Sangat berminat untuk mempelajari komputer, menginstalasi dan trouble shooting komputer secara software maupun hardware'
             ],
             [
-                'id_prodi' => 10,
-                'id_kriteria' => 1,
-                'pertanyaan' => 'Menyukai mesin dan hal-hal yang berkaitan dengan teknik'
-            ],
-            [
-                'id_prodi' => 10,
+                'id_prodi' => 3,
                 'id_kriteria' => 2,
-                'pertanyaan' => 'Memiliki keahlian dalam perawatan mesin serta bidang keahlian fisika dan matematika'
+                'pertanyaan' => 'Mempunyai kemampuan dasar dalam menginstalasi dan trouble shooting komputer secara software dan hardware'
             ],
             [
-                'id_prodi' => 10,
+                'id_prodi' => 3,
                 'id_kriteria' => 3,
-                'pertanyaan' => 'Hobi dalam merakit, merawat dan pembelajaran tentang mesin'
+                'pertanyaan' => 'Hobi melakukan kegiatan yang berkaitan dengan komputer dan jaringan'
             ],
             [
                 'id_prodi' => 4,
@@ -81,7 +76,203 @@ class PertanyaanSeeder extends Seeder
                 'id_prodi' => 4,
                 'id_kriteria' => 3,
                 'pertanyaan' => 'Hobi melakukan pembuatan laporan keuangan, melakukan analisis pasar keuangan dan senang berbisnis'
-            ]
+            ],
+            [
+                'id_prodi' => 5,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam ilmu perhitungan, sistem keuangan dan etiket bisnis'
+            ],
+            [
+                'id_prodi' => 5,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dasar matematika dan software akuntansi'
+            ],
+            [
+                'id_prodi' => 5,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi melakukan pembuatan laporan keuangan, melakukan analisis pasar keuangan dan senang berbisnis'
+            ],
+            [
+                'id_prodi' => 6,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari bahasa asing.'
+            ],
+            [
+                'id_prodi' => 6,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dasar dalam mengingat dan cepat dalam belajar bahasa asing.'
+            ],
+            [
+                'id_prodi' => 6,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi berbicara bahasa asing serta menyenangi hal-hal yang berkaitan dengan bahasa asing.'
+            ],
+            [
+                'id_prodi' => 7,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari tentang bagaimana merancang, membangun, merenovasi gedung dan infrastruktur lainya juga mencakup lingkungan.'
+            ],
+            [
+                'id_prodi' => 7,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dasar tentang infrastruktur bangunan dan lingkungan.'
+            ],
+            [
+                'id_prodi' => 7,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi dalam membangun, merenovasi, dan merancang suatu gedung atau infrastruktur lainya. '
+            ],
+            [
+                'id_prodi' => 8,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari tentang bagaimana merancang, membangun, merenovasi gedung dan infrastruktur lainya juga mencakup lingkungan.'
+            ],
+            [
+                'id_prodi' => 8,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dasar tentang infrastruktur bangunan dan lingkungan.'
+            ],
+            [
+                'id_prodi' => 8,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi dalam membangun, merenovasi, dan merancang suatu gedung atau infrastruktur lainya. '
+            ],
+            [
+                'id_prodi' => 9,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari tentang bagaimana merancang, membangun, merenovasi gedung dan infrastruktur lainya juga mencakup lingkungan.'
+            ],
+            [
+                'id_prodi' => 9,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dasar tentang infrastruktur bangunan dan lingkungan.'
+            ],
+            [
+                'id_prodi' => 9,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi dalam membangun, merenovasi, dan merancang suatu gedung atau infrastruktur lainya. '
+            ],
+            [
+                'id_prodi' => 10,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Menyukai mesin dan hal-hal yang berkaitan dengan teknik'
+            ],
+            [
+                'id_prodi' => 10,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dalam perawatan mesin serta bidang keahlian fisika dan matematika'
+            ],
+            [
+                'id_prodi' => 10,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi dalam merakit, merawat dan pembelajaran tentang mesin'
+            ],
+            [
+                'id_prodi' => 11,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari ilmu fisika dalam praktek mesin-mesin besar yang berfungsi untuk kegiatan konstruksi.'
+            ],
+            [
+                'id_prodi' => 11,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dasar tentang alat-alat berat dan keselamatan kerja.'
+            ],
+            [
+                'id_prodi' => 11,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi dalam menggunakan dan mengoperasikan alat-alat berat. '
+            ],
+            [
+                'id_prodi' => 12,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari tentang komponen listrik dan alat-alat semi konduktor.'
+            ],
+            [
+                'id_prodi' => 12,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dasar tentang kelistrikan dan alat-alat elektronik.'
+            ],
+            [
+                'id_prodi' => 12,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi berkegiatan yang berkaitan dengan kelistrikan. '
+            ],
+            [
+                'id_prodi' => 13,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari tentang komponen listrik dan alat-alat semi konduktor.'
+            ],
+            [
+                'id_prodi' => 13,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dasar tentang kelistrikan dan alat-alat elektronik.'
+            ],
+            [
+                'id_prodi' => 13,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi berkegiatan yang berkaitan dengan kelistrikan. '
+            ],
+            [
+                'id_prodi' => 14,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari tentang komponen listrik dan alat-alat semi konduktor.'
+            ],
+            [
+                'id_prodi' => 14,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki keahlian dasar tentang kelistrikan dan alat-alat elektronik.'
+            ],
+            [
+                'id_prodi' => 14,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi berkegiatan yang berkaitan dengan kelistrikan. '
+            ],
+            [
+                'id_prodi' => 15,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari segala sesuatu yang terkait dengan kegiatan operasional bisnis dan perusahaan.'
+            ],
+            [
+                'id_prodi' => 15,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki kemampuan dasar manajerial, komunasi, dan analisa yang bagus. '
+            ],
+            [
+                'id_prodi' => 15,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi dalam mengurus dan mengelola file-file yang berkaitan dengan perusahaan dan bisnis.'
+            ],
+            [
+                'id_prodi' => 15,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari ilmu pengelolaan perjalanan wisata, pemandu wisata, maupun layanan kehumasan.'
+            ],
+            [
+                'id_prodi' => 16,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki kemampuan komunikasi yang bagus.'
+            ],
+            [
+                'id_prodi' => 16,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi dalam mengurus dan memandu perjalana wisata. '
+            ],
+            [
+                'id_prodi' => 17,
+                'id_kriteria' => 1,
+                'pertanyaan' => 'Berminat dalam mempelajari segala sesuatu yang terkait dengan kegiatan operasional bisnis dan perusahaan.'
+            ],
+            [
+                'id_prodi' => 17,
+                'id_kriteria' => 2,
+                'pertanyaan' => 'Memiliki kemampuan dasar manajerial, komunasi, dan analisa yang bagus. '
+            ],
+            [
+                'id_prodi' => 17,
+                'id_kriteria' => 3,
+                'pertanyaan' => 'Hobi dalam mengurus dan mengelola file-file yang berkaitan dengan perusahaan dan bisnis.'
+            ],
+            
 
 
             ];
